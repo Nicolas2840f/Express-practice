89b89471b2a5ea4b507e723512d7bfacf24cee41
Nicolas2840f <ncastaneda840@misena.edu.co>
2024-05-13 12:07:36 -0500

    Primer push

---
+++ phonebook/index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+
+let persons = [
+  {
+    name: "Arto Hellas",
+    number: "040-123456",
+    id: "1",
+  },
+  {
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+    id: "2",
+  },
+  {
+    name: "Dan Abramov",
+    number: "12-43-234345",
+    id: "3",
+  },
+];
+
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
+});
+
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
+
+app.get("/info",(request,response)=>{
+    const currentTime = new Date();
+    response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${currentTime}</p>`)
+});
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
